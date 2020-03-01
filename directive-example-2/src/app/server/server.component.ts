abf3584972e05918f83fc48a0fa9e0ab2eef99fe
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-03-01 23:31:36 +1100

    Add an example that uses ngStyle attribute directive

---
+++ directive-example-2/src/app/server/server.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+    serverId:number = 10;
+    serverStatus:string = 'Offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'Online' ? 'green' : 'red';
+    }
+}
