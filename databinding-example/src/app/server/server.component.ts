8d3628596dbad5d2b830d23b031593b831dd71b9
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-03-01 14:11:47 +1100

    Use a method in data binding example

--- databinding-example/src/app/server/server.component.ts
+++ databinding-example/src/app/server/server.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId:number = 10;
     serverStatus:string = 'Offline';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
