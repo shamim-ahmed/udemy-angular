019f4fe74b16887cfe290fd22f4b8b3545a46466
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-03-01 15:26:19 +1100

    Add another example of event binding

--- databinding-example/src/app/servers/servers.component.ts
+++ databinding-example/src/app/servers/servers.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = 'No server was created!'
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() { 
     setTimeout(() => {
@@ -19,6 +20,10 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'A server was created successfully !';
+    this.serverCreationStatus = 'Server was created successfully!';
+  }
+
+  onChangeServerName(event:Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
